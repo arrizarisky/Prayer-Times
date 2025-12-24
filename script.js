ca1de8e75d93a525c87dfb3de09cb2b01673e29b
arrizarisky <mhdarriza1404@gmail.com>
2025-12-24 09:39:44 +0700

    `Update index.html and script.js to display prayer times based on user location`

--- script.js
+++ script.js
@@ -8,31 +8,60 @@ function prayerTimes(latitude, longitude) {
   )
     .then((response) => response.json())
     .then(function (response) {
-      console.log(response.data[1][0].timings);
+      let date = new Date();
+      let today = date.getDate - 1;
+      const data = response.data[1][0].timings;
+
+      let app = document.getElementById("app");
+      let table = document.getElementById("table");
+      let tbody = document.getElementById("tableBody");
+
+      for (i in data) {
+        let row = tbody.insertRow();
+        let name = row.insertCell(0);
+        let time = row.insertCell(1);
+        name.innerHTML = i;
+        time.innerHTML = data[i];
+        tbody.appendChild(row);
+      }
+      table.appendChild(tbody);
+      app.appendChild(table);
     });
 }
 
-function success(position) {
-  prayerTimes(position.coords.latitude, position.coords.longitude);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("getLocation");
 
-function error() {
-  alert("Lokasi tidak dapat dijangkau");
-}
-
-function getUserLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    alert("Dibutuhkan lokasi untuk Prayer Times");
+  if (!btn) {
+    console.error("Tombol getLocation tidak ditemukan");
+    return;
   }
-}
+
+  btn.addEventListener("click", getUserLocation);
+
+  function getUserLocation() {
+    alert("Lokasi anda terdeteksi");
+
+    if (!navigator.geolocation) {
+      alert("Browser tidak mendukung geolocation");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  function success(position) {
+    prayerTimes(position.coords.latitude, position.coords.longitude);
+
+    btn.style.display = "none";
+  }
+
+  function error() {
+    alert("Lokasi tidak dapat dijangkau");
+  }
+});
 
 function main() {
-  let app = document.getElementById("app");
-  let Title = document.createElement("h1");
-  Title.innerHTML = "Prayer Times";
-  app.appendChild(Title);
   getUserLocation();
 }
 main();
